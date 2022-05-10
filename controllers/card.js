f1ff5c33770c04e1938e0de3231c26b9abbb5859
Maryna Doroshenko <verymarine@MacBook-Pro.local>
2022-05-10 05:16:03 +0300

    Project13

--- controllers/card.js
+++ controllers/card.js
@@ -41,10 +41,10 @@ module.exports.postCard = async (req, res) => {
 module.exports.deleteCard = async (req, res) => {
   try {
     const card = await Card.findByIdAndRemove(req.params._id);
-    if (card) {
-      res.status(200).send(card);
-    } else {
+    if (!card.owner !== req.user._id) {
       res.status(404).send({ message: 'Пользователь по указанному _id не найден' });
+    } else {
+      res.status(200).send(card);
     }
   } catch (err) {
     res.status(500).send({ message: err.message });
