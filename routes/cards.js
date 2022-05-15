3c1b70b73d857865a99f610edb1b0fea9d457802
Maryna Doroshenko <verymarine@MacBook-Pro.local>
2022-05-16 02:00:08 +0300

    Project14

--- routes/cards.js
+++ routes/cards.js
@@ -7,7 +7,7 @@ const {
 router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().require().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(/https?:\/\/(www\.)?[-\w@:%\\.\\+~#=]{1,256}\.[a-z0-9()]{1,6}\b([-\w()@:%\\.\\+~#=//?&]*)/i),
   }),
 }), postCard);
